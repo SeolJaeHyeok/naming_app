30d0fffdce7c44b0fd2739b69e624ed1a6b9c733
seoljaehyeok <milkboy2564@naver.com>
2020-11-10 20:05:32 +0900

    List로 랜덤영어 단어 받아온 후 출력

---
+++ android/app/src/main/kotlin/example/flutter_naming_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package example.flutter_naming_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/random_list.dart
@@ -0,0 +1,45 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class RandomList extends StatefulWidget {
+  @override
+  _RandomListState createState() => _RandomListState();
+}
+
+class _RandomListState extends State<RandomList> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Naming App'),
+      ),
+      body: _buildList(),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(itemBuilder: (context, index) {
+      // 0, 2, 4, 6, 8 .... -> real item
+      // 1, 3, 5, 7, 9 .... -> dividers
+      if (index.isOdd) {
+        return Divider();
+      }
+
+      var realIndex = index ~/ 2;
+
+      if (realIndex >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[realIndex]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return  ListTile(
+        title:
+        Text(pair.asPascalCase, textScaleFactor: 1.5));
+  }
+}
