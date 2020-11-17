40f8a7bc5ec4428591c78d5527224bb6966bf0e8
seoljaehyeok <milkboy2564@naver.com>
2020-11-17 17:52:43 +0900

    Bloc 패턴과 Stream 적용

--- lib/src/random_list.dart
+++ lib/src/random_list.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naming_app/src/savedList.dart';
+import 'bloc/bloc.dart';
 
 class RandomList extends StatefulWidget {
   @override
@@ -9,7 +10,6 @@ class RandomList extends StatefulWidget {
 
 class _RandomListState extends State<RandomList> {
   final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +21,7 @@ class _RandomListState extends State<RandomList> {
               icon: Icon(Icons.list),
               onPressed: () {
                 Navigator
-                    .push(context, MaterialPageRoute(builder: (context) => SavedList(saved : _saved)));
+                    .push(context, MaterialPageRoute(builder: (context) => SavedList()));
               }),
         ],
       ),
@@ -30,24 +30,29 @@ class _RandomListState extends State<RandomList> {
   }
 
   Widget _buildList() {
-    return ListView.builder(itemBuilder: (context, index) {
-      // 0, 2, 4, 6, 8 .... -> real item
-      // 1, 3, 5, 7, 9 .... -> dividers
-      if (index.isOdd) {
-        return Divider();
-      }
+    return StreamBuilder<Set<WordPair>>(
+      stream: bloc.savedStream,
+      builder: (context, snapshot) {
+        return ListView.builder(itemBuilder: (context, index) {
+          // 0, 2, 4, 6, 8 .... -> real item
+          // 1, 3, 5, 7, 9 .... -> dividers
+          if (index.isOdd) {
+            return Divider();
+          }
 
-      var realIndex = index ~/ 2;
+          var realIndex = index ~/ 2;
 
-      if (realIndex >= _suggestions.length) {
-        _suggestions.addAll(generateWordPairs().take(10));
+          if (realIndex >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(snapshot.data, _suggestions[realIndex]);
+        });
       }
-      return _buildRow(_suggestions[realIndex]);
-    });
+    );
   }
 
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
+  Widget _buildRow(Set<WordPair> saved, WordPair pair) {
+    final bool alreadySaved = saved == null ? false : saved.contains(pair);
 
     return ListTile(
       title: Text(pair.asPascalCase, textScaleFactor: 1.5),
@@ -56,14 +61,8 @@ class _RandomListState extends State<RandomList> {
         color: Colors.pink,
       ),
       onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair); // true
-          } else {
-            _saved.add(pair); // false
-          }
-        });
-        print(_saved.toString()); // 확인용
+        bloc.addToOrRemoveFromSavedList(pair);
+        print(saved.toString()); // 확인용
       },
     );
   }
--- lib/src/savedList.dart
+++ lib/src/savedList.dart
@@ -1,11 +1,9 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naming_app/src/random_list.dart';
+import 'bloc/bloc.dart';
 
 class SavedList extends StatefulWidget {
-  SavedList({@required this.saved});
-
-  final Set<WordPair> saved;
 
   @override
   _SavedListState createState() => _SavedListState();
@@ -23,24 +21,33 @@ class _SavedListState extends State<SavedList> {
   }
 
   Widget _buildList() {
-    return ListView.builder(
-        itemCount: widget.saved.length * 2, // divider도 인덱스에 포함되므로 2배를 해줘야
-        itemBuilder: (context, index) {
-          if (index.isOdd) return Divider();
+    return StreamBuilder<Set<WordPair>>(
+      stream: bloc.savedStream,
+      builder: (context, snapshot) {
+        var saved = Set<WordPair>();
+        if(snapshot.hasData)
+          saved.addAll(snapshot.data);
+        else
+          bloc.addCurrentSaved;
 
-          var realIndex = index ~/ 2;
+        return ListView.builder(
+            itemCount: saved.length * 2, // divider도 인덱스에 포함되므로 2배를 해줘야
+            itemBuilder: (context, index) {
+              if (index.isOdd) return Divider();
 
-          return _buildRow(widget.saved.toList()[realIndex]);
-        });
+              var realIndex = index ~/ 2;
+
+              return _buildRow(saved.toList()[realIndex]);
+            });
+      }
+    );
   }
 
   Widget _buildRow(WordPair pair) {
     return ListTile(
       title: Text(pair.asPascalCase, textScaleFactor: 1.5),
       onTap: () {
-        setState(() {
-          widget.saved.remove((pair));
-        });
+        bloc.addToOrRemoveFromSavedList(pair);
       },
     );
   }
